b6de2fc39b27809627b6a09aacc17f2b6edd4272
Yusuf Ertas <yusufertas@hotmail.com>
2019-02-01 22:24:44 +0300

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,22 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
+    if (err){
+       return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    // db.collection('Todos').find({_id:new ObjectID('5c54957af6d0341a18fdb9a1')}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs));
+    // },(err) => {
+    //     console.log('Unable to feth todos',err)
+    // });
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    },(err) => {
+        console.log('Unable to feth todos',err)
+    });
+    
+    // db.close();
+});
