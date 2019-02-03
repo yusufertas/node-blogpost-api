07ebd755ffbbde70d46c94c104a3032889b896b6
Yusuf Ertas <yusufertas@hotmail.com>
2019-02-03 21:48:04 +0300

    committing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://yusufertas:R1_nappe@ds129904.mlab.com:29904/yusufertas-blogposts' || 'mongodb://localhost:27017/blogPosts');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/blogPosts');
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {blogPost} = require('./models/blogPost');
 
 var app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.post('/blogPosts',(req,res) => {
     var newBlogPost2 = new blogPost({
@@ -42,7 +42,7 @@ app.get('/blogPosts/:id', (req,res) => {
         res.send({blogposts});
     }).catch((e) => {
         res.status(400).send();
-    })
+    });
 });
 
 
@@ -51,5 +51,4 @@ app.listen(port, () => {
 });
 
 module.exports = {app};
-
 
