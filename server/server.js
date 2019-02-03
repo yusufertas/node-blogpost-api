849f2f7d9b507e2f168e17d0e9580169a9f66298
Yusuf Ertas <yusufertas@hotmail.com>
2019-02-03 18:40:52 +0300

    queries playground

--- server/server.js
+++ server/server.js
@@ -35,16 +35,5 @@ app.listen(3000, () => {
 });
 
 module.exports = {app};
-// var newBlogPost1 = new blogPost({
-//     content:"The world as we have created it is a process of our thinking. It cannot be changed without changing our thinking.",
-//     author:"Albert Einstein"
-// });
 
-// newBlogPost1.tags.push(['change', 'deep-thoughts', 'thinking', 'world']);
-
-// newBlogPost1.save().then((doc) => {
-//     console.log(JSON.stringify(doc));
-// },(e) => {
-//     console.log('Unable to save blog post',e);
-// });
 
