b310e92fd832ae745607626891b1837717e1bf5a
Yusuf Ertas <yusufertas@hotmail.com>
2019-02-05 19:31:37 +0300

    Add PATCH /blogPosts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express =require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash')
+const express =require('express');
+const bodyParser = require('body-parser');
 
-var {ObjectID} = require('mongodb');
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {blogPost} = require('./models/blogPost');
 
@@ -58,6 +59,22 @@ app.delete('/blogPosts/:id', (req,res) => {
     }).catch((e) => {
         res.status(400).send();
     })
+});
+
+app.patch('/blogPosts/:id',(req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body,['content','author','tags']);
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    blogPost.findOneAndUpdate(id,{$set:body},{new:true}).then((blogposts) =>{
+        if (!blogposts){
+            return res.status(404).send();
+        }
+        res.send({blogposts});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 const port = 3000 || process.env.PORT;
